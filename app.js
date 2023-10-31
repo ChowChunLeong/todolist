4678730464dfac4405b38996d4bb41cc41b8a036
shubhamatkal <shubhamatkal333@gmail.com>
2023-10-31 17:16:44 +0530

    change ide

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 
+
 app.get("/", (req, res)=>{
     let today = new Date();
     var options = { weekday: 'long', year: 'numeric',
      month: 'long', day: 'numeric' };
-    console.log(today.toLocaleDateString("en-US",options));
-    res.send("Hello India");
+//    console.log(today.toLocaleDateString("en-US",options));
+    var today_ = today.toLocaleDateString("en-US",options);
+    res.render("list.ejs",{day:today_} )
+})
+
+app.post("/", (req, res)=>{
+    let new_ = req.body.newitem;
+    res.redirect("/");
+
+
 })
 
 
