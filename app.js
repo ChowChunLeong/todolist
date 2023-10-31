b253cea704f9d61748a8773a3bd322de56540cac
shubhamatkal <shubhamatkal333@gmail.com>
2023-10-31 16:22:47 +0530

    fetch todays date

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.get("/", (req, res)=>{
+    let today = new Date();
+    var options = { weekday: 'long', year: 'numeric',
+     month: 'long', day: 'numeric' };
+    console.log(today.toLocaleDateString("en-US",options));
+    res.send("Hello India");
+})
+
+
 
 app.listen(port, ()=>{
     console.log(`Running at ${port}.`)
