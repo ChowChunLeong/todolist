153795cc5bfb101947b8a2913d1a091a67d5ed3d
shubhamatkal <shubhamatkal333@gmail.com>
2023-10-31 17:30:51 +0530

    last commit for today

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 
-
+let new_items_list = [];
 app.get("/", (req, res)=>{
     let today = new Date();
     var options = { weekday: 'long', year: 'numeric',
      month: 'long', day: 'numeric' };
 //    console.log(today.toLocaleDateString("en-US",options));
     var today_ = today.toLocaleDateString("en-US",options);
-    res.render("list.ejs",{day:today_} )
+    res.render("list.ejs",{day:today_, items : new_items_list} )
 })
 
 app.post("/", (req, res)=>{
     let new_ = req.body.newitem;
+    new_items_list.push(new_);
     res.redirect("/");
 
 
